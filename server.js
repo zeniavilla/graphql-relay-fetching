d6ab075e7824a30a665660324d9c719f09b0c70f
Zenia <zeniavilla@hotmail.com>
2017-10-11 10:37:19 -0700

    Setup schema with hello query

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('GraphQL & Relay modern is cool.');
 });
+
+const root = { hello: () => "Hi, I'm Z" };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(8080, () => console.log('Running server on localhost:8080/graphql'));
