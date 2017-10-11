a7c044a93b794183ff035b15bc0d2fe2ead37b94
Zenia <zeniavilla@hotmail.com>
2017-10-11 12:02:41 -0700

    Finish schema and estabilsh connection between friend and GraphQL friend and user and GraphQL user

--- data/schema.js
+++ data/schema.js
@@ -23,12 +23,32 @@ import {
   getViewer,
 } from './database';
 
+const {nodeInterface, nodeField} = nodeDefinitions(
+  (globalId) => {
+    const {type, id} = fromGlobalId(globalId);
+    if (type === 'Friend') {
+      return getFriend(id);
+    } else if (type === 'User') {
+      return getUser(id);
+    }
+    return null;
+  },
+  (obj) => {
+    if (obj instanceof Friend) {
+      return GraphQLFriend;
+    } else if (obj instanceof User) {
+      return GraphQLUser;
+    }
+    return null;
+  }
+);
+
 const GraphQLUser = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
     id: globalIdField('User'),
     friends: {
-      type: friendConnection,
+      type: friendsConnection,
       args: connectionArgs,
       resolve: (_, args) => connectionFromArray(getFriends(), args),
     },
@@ -60,4 +80,22 @@ const GraphQLFriend = new GraphQLObjectType({
     },
   }),
   interface: [nodeInterface],
-})
+});
+
+const { connectionType: friendsConnection } =
+  connectionDefinitions({ name: 'Friend', nodeType: GraphQLFriend });
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    node: nodeField,
+    viewer: {
+      type: GraphQLUser,
+      resolve: () => getViewer(),
+    },
+  }),
+});
+
+export const schema = new GraphQLSchema({
+  query: Query,
+});
