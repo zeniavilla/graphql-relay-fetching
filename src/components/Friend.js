0a398803d2b2c0b4430e3c3041952d4b334832cf
Zenia <zeniavilla@hotmail.com>
2017-10-11 14:05:02 -0700

    Add friend content in divs

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -4,8 +4,26 @@ import { createFragmentContainer, graphql } from 'react-relay';
 class Friend extends Component {
   render() {
     return (
-      <li>
-        {this.props.friend.firstName} {this.props.friend.lastName}
+      <li className="col l6 s12">
+        <div className="card">
+          <div className="card-image">
+            <img src={this.props.friend.image} alt={this.props.friend.firstName} />
+            <span className="card-title">{this.props.friend.firstName} {this.props.friend.lastName}</span>
+          </div>
+          <div className="card-content">
+            <div className="row">
+              <div className="col l6 center">
+                <blockquote>Is a {this.props.friend.gender}</blockquote>
+              </div>
+              <div className="col l6 center">
+                <blockquote>Speaks {this.props.friend.language}</blockquote>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <a href={`mailto:${this.props.friend.email}`} target="_blank">Email: {this.props.friend.email}</a>
+          </div>
+        </div>
       </li>
     )
   }
