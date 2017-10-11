dea1bd810f2d9f0c9834ab389d41f2193f237190
Zenia <zeniavilla@hotmail.com>
2017-10-11 12:33:21 -0700

    Create Friends component

---
+++ src/components/Friends.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createFragmentContainer, graphql } from 'react-relay';
+
+class Friends extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <h1>Friends</h1>
+          <FriendsList viewer={this.props.viewer} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default createFragmentContainer(Friends, {
+  viewer: graphql`
+    fragment Friends_viewer on User {
+      id,
+      ...FriendsList_viewer,
+    }
+  `,
+});
