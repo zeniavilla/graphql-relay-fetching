5acbf969f9405ee8edd8bb84d1e1ddfa6d62668b
Zenia <zeniavilla@hotmail.com>
2017-10-11 12:25:22 -0700

    Create network layer in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryRenderer, graphql } from 'react-relay';
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
 
 import Friends from './components/Friends';
+
+// Network layer
+function fetchQuery( operation, variables) {
+  return fetch('/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  });
+}
 
 const mountNode = document.getElementById('root');
 
