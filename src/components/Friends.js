b7b6a488924bc607fd7a2096b2828da7d0a45819
Zenia <zeniavilla@hotmail.com>
2017-10-11 12:53:02 -0700

    Remove ReactDOM import

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import FriendsList from './FriendsList';
 import { createFragmentContainer, graphql } from 'react-relay';
 
--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Friend from './Friend';
 import { createFragmentContainer, graphql } from 'react-relay';
 
